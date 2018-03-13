2210cfd61b4a3be34fc7b64977d102770ec4658c
Sondre Gjellestad <sondre.gjellestad@hotmail.com>
2018-03-13 06:09:38 +0100

    :sparkles: Oppgave 2

---
+++ Kode/Oppgave 2/src/main/java/no/hvl/dat102/oppg2/adt/DobbelKjedetOrdnetListeMADT.java
@@ -0,0 +1,27 @@
+package no.hvl.dat102.oppg2.adt;
+
+public interface DobbelKjedetOrdnetListeMADT<T> {
+ 
+ /**
+  * 
+  * @param el er elementet som skal legges til
+  */
+  void leggTil(T el);
+  
+  
+  /**
+   * 
+   * @param el er elementet det søkes etter
+   * @return true hvis elementet fins ellers false
+   */    
+  boolean fins(T el);
+  
+  /**
+   * 
+   * @param el er elementet som skal fjernes hvis det fins
+   * @return elementet som skal fjernes ellers returneres null
+   */  
+  T fjern(T el);
+  
+
+}
